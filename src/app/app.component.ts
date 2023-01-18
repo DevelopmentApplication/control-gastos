195af06cc1467a1c63544d513082fd0b72cfa653
Gabriel Bustos Pizarro <gabriel.bustos@wom.cl>
2023-01-18 02:54:30 -0300

    config prod

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
+import { environment } from 'src/enviroments/enviroment';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'control_gastos';
+  env = environment.env;
 }
