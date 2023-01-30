49cceb8a9b84522105bb40071b8cc6b0180cf1c8
Gabriel Bustos Pizarro <gabriel.bustos@wom.cl>
2023-01-30 15:17:09 -0300

    change apitoken

--- src/environments/environment.dev.ts
+++ src/environments/environment.dev.ts
@@ -3,7 +3,7 @@ export const environment = {
   env: 'develop',
   RESTservices: {
     apiToken:
-      '87cef25a3b73beafa67e56c671a78b0c3c818cdac91a8fd87ec1c85ec8303da45145a68a5c7a410a8fa3d117998e4362c47026e0294c09869282eab6926564a5b083603af3b89de2e0cb304d72167309fb49e7d6934c5ff55c1a79210d57fd2c9f18138bdfda1a4a6abf6892886e8ff7003deaf57169b49b2901320e629e94ce',
+      'e3a2ba08a8e64f2058fdd47f57d046f45c1dae1a836090a545641baf238a533beeab75a2117c47696f36a84de4bec77a266ee1815ab949c792300a39247a3d6525c71b5ceed16fc8473d68002b086fbcebfa6dcbb4ff43dd0fdf25df4421ce9fa6561a6f03226a7f90e5a1f9f4b522adf852135c35bbab46e1738fce051ea7c2',
     baseUrl:
       'https://backcontrolgastos-dot-control-gastos-dev.ew.r.appspot.com/',
     authentication: {
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,7 @@ export const environment = {
   env: 'local',
   RESTservices: {
     apiToken:
-      '853884d3f462f3385cf601466fde523b92981b10ae32bbae35fb41cb440e99bd4e37bf5d2fb084b0f092ea40ce8c4ad269a3b237bb6378f5638f580ec3f466886b12b05509a1f3ddef4f0d1b6b3e6d2ecf28e9099b94e0599711e7c2c5b260ce955f53f6511126e8d2f5bfceb63d245107dd4d00dbd72bac5263e2a1b629ff01',
+      'e3a2ba08a8e64f2058fdd47f57d046f45c1dae1a836090a545641baf238a533beeab75a2117c47696f36a84de4bec77a266ee1815ab949c792300a39247a3d6525c71b5ceed16fc8473d68002b086fbcebfa6dcbb4ff43dd0fdf25df4421ce9fa6561a6f03226a7f90e5a1f9f4b522adf852135c35bbab46e1738fce051ea7c2',
     baseUrl: 'http://localhost:1337',
     authentication: {
       auth: '/api/auth/local',
